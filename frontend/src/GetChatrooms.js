deacf0bdf1d888dc7533dc09b52f0f33e4ef3fa6
koenigalpdx <koenigal@pdx.edu>
2025-02-18 15:05:38 -0800

    updated
    e

--- frontend/src/GetChatrooms.js
+++ frontend/src/GetChatrooms.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 export const getChatrooms = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/chatrooms");
-      return res.data;
+        const res = await axios.get("http://localhost:5000/api/chatrooms");
+        return res.data;
     } catch (err) {
-      console.error("Error fetching chatrooms:", err.response?.data || err.message);
+        console.error("Error fetching chatrooms:", err.response?.data || err.message);
     }
-  };  
+};  
+
+export default getChatrooms
